d205238f5864c1bcaf870d34ed6f4372d1b37fef
Jacob Millner <jacob.millner@outlook.com>
2019-01-12 01:23:27 -0500

    switching to better-sqlite3

--- models/database.js
+++ models/database.js
@@ -1,12 +1,7 @@
-const sqlite3 = require("sqlite3").verbose();
+const SQLite = require("better-sqlite3");
+const sql = new SQLite('../SmashBot.sqlite');
 
 module.exports.Housekeeping = function (callback) {
-    const sql = new sqlite3.Database('../SmashBot.db', sqlite3.OPEN_READWRITE, (err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log('Connected to the SmashBot database.');
-    });
     // Check if the tables exist.
     const rankingTableCheck = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'Rankings';").get();
     const usersTableCheck = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'Users';").get();
@@ -17,7 +12,9 @@ module.exports.Housekeeping = function (callback) {
         // If the table isn't there, create it and setup the database correctly.
         sql.prepare("CREATE TABLE Users (id TEXT PRIMARY KEY, UserName TEXT, DateCreated DATETIME default current_timestamp);").run();
         // Ensure that the "id" row is always unique and indexed.
-        sql.prepare("CREATE UNIQUE INDEX idx_scores_id ON Users (id);").run();
+        sql.prepare("CREATE UNIQUE INDEX idx_Users_id ON Users (id);").run();
+        sql.pragma("synchronous = 1");
+        sql.pragma("journal_mode = wal");
 
         log += 'Table: Users not found! Creating Table now...';
     }
@@ -26,14 +23,14 @@ module.exports.Housekeeping = function (callback) {
         // If the table isn't there, create it and setup the database correctly.
         sql.prepare("CREATE TABLE Rankings (id TEXT PRIMARY KEY, Winner_Id INTEGER, Loser_Id INTEGER, DateCreated DATETIME default current_timestamp);").run();
         // Ensure that the "id" row is always unique and indexed.
-        sql.prepare("CREATE UNIQUE INDEX idx_scores_id ON Rankings (id);").run();
+        sql.prepare("CREATE UNIQUE INDEX idx_Rankings_id ON Rankings (id);").run();
+        sql.pragma("synchronous = 1");
+        sql.pragma("journal_mode = wal");
 
         log += '\nTable: Rankings not found! Creating Table now...';
     }
 
     log += '\nDatabase is ready!'
-
-    sql.close();
 
     //were done here, run callback
     callback(null, log);
--- models/users.js
+++ models/users.js
@@ -1,16 +1,9 @@
-const sqlite3 = require("sqlite3").verbose();
+const SQLite = require("better-sqlite3");
+const sql = new SQLite('../SmashBot.sqlite');
 
 module.exports.GetAllUsers = function () {
-    const sql = new sqlite3.Database('../SmashBot.db', sqlite3.OPEN_READWRITE, (err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log('Connected to the SmashBot database.');
-    });
     //TODO 
     const rankings = sql.prepare("SELECT * FROM Users").get();
-
-    sql.close();
     
     //TODO return rankings
 }
