ffb28c0fedcdd393c8427d8053ff3e900bc04b96
Jacob Millner <jacob.millner@outlook.com>
2019-01-13 17:35:50 -0500

    a bit of refactoring

--- smashbot.js
+++ smashbot.js
@@ -12,40 +12,41 @@ client.botlog = botlog;
 
 client.on('ready', () => {
     Database.Housekeeping((err, log) => {
-        console.log(log);
+        botlog.log(log);
     });
-    console.log(`Logged in as ${client.user.tag}!`);
-    console.log('Becky, please.');
+    botlog.log(`Logged in as ${client.user.tag}!`);
+    botlog.log('Becky, please.');
 });
 
 // This loop reads the /events/ folder and attaches each event file to the appropriate event.
 fs.readdir("./events/", (err, files) => {
     if (err) return console.error(err);
     files.forEach(file => {
-        // If the file is not a JS file, ignore it (thanks, Apple)
-        if (!file.endsWith(".js")) return;
-        // Load the event file itself
-        const event = require(`./events/${file}`);
-        // Get just the event name from the file name
-        let eventName = file.split(".")[0];
-        client.on(eventName, event.bind(null, client));
-        delete require.cache[require.resolve(`./events/${file}`)];
+        // If the file is not a JS file, ignore it
+        if (file.endsWith(".js")) {
+            // Load the event file itself
+            const event = require(`./events/${file}`);
+            // Get just the event name from the file name
+            let eventName = file.split(".")[0];
+            client.on(eventName, event.bind(null, client));
+            delete require.cache[require.resolve(`./events/${file}`)];
+        }
     });
 });
 
 client.commands = new Enmap();
-
 fs.readdir("./commands/", (err, files) => {
     if (err) return console.error(err);
     files.forEach(file => {
-        if (!file.endsWith(".js")) return;
-        // Load the command file itself
-        let props = require(`./commands/${file}`);
-        // Get just the command name from the file name
-        let commandName = file.split(".")[0];
-        console.log(`Attempting to load command ${commandName}`);
-        // Here we simply store the whole thing in the command Enmap. We're not running it right now.
-        client.commands.set(commandName, props);
+        if (file.endsWith(".js")) {
+            // Load the command file itself
+            let props = require(`./commands/${file}`);
+            // Get just the command name from the file name
+            let commandName = file.split(".")[0];
+            botlog.log(`Attempting to load command ${commandName}`);
+            // Here we simply store the whole thing in the command Enmap. We're not running it right now.
+            client.commands.set(commandName, props);
+        }
     });
 });
 
